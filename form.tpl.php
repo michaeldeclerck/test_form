37c2453a9e56c813fa19a11b8f0fa63a30f20d50
michaeldeclerck <michael.de.clerck@dutchlearning.com>
2015-03-22 18:11:46 +0100

    Added form template
    Added the form.tpl.php file as a template for use in this test.

---
+++ form.tpl.php
@@ -0,0 +1,13 @@
+<h1>Test form</h1>
+
+<form method="POST" action="index.php">
+	<input type=input name=val1 value=0 size=4> 
+	+
+	<input type=input name=val2 value=0 size=4>
+	=
+	<input type=input name=result value=0 size=4>
+	
+	<br />
+	
+	<input type=submit name=submit value=Calculate>
+</form>
